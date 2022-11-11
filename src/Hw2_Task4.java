cb39885c97924a3358b62648ddc755cec2fe1b66
AllaKulchitskaya <alla070@list.ru>
2022-11-11 21:18:29 +0300

    Дз 2 - Задача 4

---
+++ src/Hw2_Task4.java
@@ -0,0 +1,7 @@
+public class Hw2_Task4 {
+    public static void main(String[] args) {
+        for (int dayForFriday = 4; dayForFriday <= 31; dayForFriday = dayForFriday + 7) {
+            System.out.println("Сегодня пятница, " + dayForFriday + "-е число. Необходимо подготовить отчет");
+        }
+    }
+}
