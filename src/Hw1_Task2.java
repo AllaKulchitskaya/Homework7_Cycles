0ac67fa94c10fcd53850f88c0c204da47ed3ef2a
AllaKulchitskaya <alla070@list.ru>
2022-11-11 18:09:21 +0300

    Дз 1 - Задача 2

---
+++ src/Hw1_Task2.java
@@ -0,0 +1,13 @@
+public class Hw1_Task2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 11) {
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        for (int y = 10; y >= 1; y--) {
+            System.out.print(y + " ");
+        }
+    }
+}
